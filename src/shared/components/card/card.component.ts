918b47860df91a7ea2554c59358b07a21b324d5d
Nikhil Dabhi <>
2025-01-15 15:47:41 +0530

    - created custom material theme - created basic components - added company logo

---
+++ src/shared/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
