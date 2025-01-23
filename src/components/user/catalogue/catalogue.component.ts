e7dfe795e8d81c3abf24b810df2229db960b92f7
Nikhil Dabhi <>
2025-01-23 19:09:53 +0530

    - home page design

--- src/components/user/catalogue/catalogue.component.ts
+++ src/components/user/catalogue/catalogue.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { fadeAnimation } from '../../../shared/animations/route-animations';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { NgxImageCompressService } from 'ngx-image-compress';
 import { NgOptimizedImage } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  computed,
+  OnInit,
+  Signal,
+  signal,
+  WritableSignal,
+  effect,
+} from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { NgxImageCompressService } from 'ngx-image-compress';
+import { fadeAnimation } from '../../../shared/animations/route-animations';
 
 @Component({
   selector: 'app-catalogue',
@@ -20,13 +33,151 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './catalogue.component.scss',
   animations: [fadeAnimation],
 })
-export class CatalogueComponent {
+export class CatalogueComponent implements OnInit {
   compressedImage!: string;
+  catalogueCatagory: WritableSignal<string> = signal('');
+  catalogueCatagories: WritableSignal<any[]> = signal([]);
+  catalogues: WritableSignal<any[]> = signal([]);
+  filteredCatalogues: Signal<any[]> = computed(() => {
+    if (this.catalogueCatagory() == 'all') {
+      return this.catalogues();
+    }
+    return this.catalogues().filter(
+      (e) => e?.category == this.catalogueCatagory()
+    );
+  });
 
   constructor(
     private http: HttpClient,
-    private imageCompress: NgxImageCompressService
-  ) {}
+    private imageCompress: NgxImageCompressService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.catalogueCatagory.set(params?.get('id') || '');
+    });
+
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras.state) {
+      const stateData: any = navigation.extras.state;
+      if (stateData?.filters) {
+        console.log({ filters: stateData?.filters });
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.catalogueCatagories.set([
+      {
+        name: 'All',
+        id: 'all',
+      },
+      {
+        name: 'Wall Collection',
+        id: 'wall-collection',
+      },
+      {
+        name: 'Floor Collection',
+        id: 'floor-collection',
+      },
+      {
+        name: 'Wooden Plank',
+        id: 'wooden-plank',
+      },
+      {
+        name: 'Parking Collection',
+        id: 'parking-collection',
+      },
+      {
+        name: 'Slab Collection',
+        id: 'slab-collection',
+      },
+      {
+        name: 'Step Raiser',
+        id: 'step-raiser',
+      },
+    ]);
+
+    this.catalogues.set([
+      {
+        img: 'assets/images/catalogues/1.jpg',
+        name: 'Wall Collection',
+        desc: '200 X 1200 mm - GVT',
+        category: 'wall-collection',
+        id: '1',
+      },
+      {
+        img: 'assets/images/catalogues/2.jpg',
+        name: 'Floor Collection',
+        desc: '1000 X 1000 mm - Full Body',
+        category: 'floor-collection',
+        id: '2',
+      },
+      {
+        img: 'assets/images/catalogues/3.jpg',
+        name: 'Slab Collection',
+        desc: '800 X 1600 mm - PGVT',
+        category: 'slab-collection',
+        id: '3',
+      },
+      {
+        img: 'assets/images/catalogues/4.jpg',
+        name: 'Wooden Plank',
+        desc: '1200 X 1200 mm - Nano',
+        category: 'wooden-plank',
+        id: '4',
+      },
+      {
+        img: 'assets/images/catalogues/5.jpg',
+        name: 'Step Raiser',
+        desc: '800 X 800 mm - Charge',
+        category: 'step-raiser',
+        id: '5',
+      },
+      {
+        img: 'assets/images/catalogues/6.jpg',
+        name: 'Wall Collection',
+        desc: '200 X 1200 mm - GVT',
+        category: 'wall-collection',
+        id: '6',
+      },
+      {
+        img: 'assets/images/catalogues/7.jpg',
+        name: 'Floor Collection',
+        desc: '1000 X 1000 mm - Full Body',
+        category: 'floor-collection',
+        id: '7',
+      },
+      {
+        img: 'assets/images/catalogues/8.jpg',
+        name: 'Slab Collection',
+        desc: '800 X 1600 mm - PGVT',
+        category: 'slab-collection',
+        id: '8',
+      },
+      {
+        img: 'assets/images/catalogues/1.jpg',
+        name: 'Wooden Plank',
+        desc: '1200 X 1200 mm - Nano',
+        category: 'wooden-plank',
+        id: '9',
+      },
+      {
+        img: 'assets/images/catalogues/2.jpg',
+        name: 'Wall Collection',
+        desc: '200 X 1200 mm - GVT',
+        category: 'wall-collection',
+        id: '10',
+      },
+      {
+        img: 'assets/images/catalogues/3.jpg',
+        name: 'Parking Collection',
+        desc: '200 X 1200 mm - GVT',
+        category: 'parking-collection',
+        id: '11',
+      },
+    ]);
+  }
 
   compressImage(imagePath: string) {
     this.http.get(imagePath, { responseType: 'blob' }).subscribe((blob) => {
--- src/components/user/home/home.component.ts
+++ src/components/user/home/home.component.ts
@@ -9,10 +9,11 @@ import {
 import { fadeAnimation } from '../../../shared/animations/route-animations';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, CommonModule],
+  imports: [MatButtonModule, CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   animations: [fadeAnimation],
@@ -20,7 +21,11 @@ import { MatButtonModule } from '@angular/material/button';
 export class HomeComponent implements OnInit {
   title = 'Bright Impex';
   images: WritableSignal<Array<any>> = signal([]);
+  trendingImages: WritableSignal<Array<any>> = signal([]);
   catalogueImages: WritableSignal<Array<any>> = signal([]);
+  productImages: WritableSignal<Array<any>> = signal([]);
+
+  // Main Slider
   currentSlideIndex: WritableSignal<number> = signal(0);
   currentSlide: Signal<any> = computed(
     () => this.images()?.[this.currentSlideIndex()]
@@ -29,22 +34,52 @@ export class HomeComponent implements OnInit {
     () => -this.currentSlideIndex() * 100
   );
 
+  // Trending Slider
+  currentTrendingSlideIndex: WritableSignal<number> = signal(0);
+  currentTrendingSlide: Signal<any> = computed(
+    () => this.trendingImages()?.[this.currentTrendingSlideIndex()]
+  );
+  currentTrendingSlideTranslate: Signal<number> = computed(
+    () => -this.currentTrendingSlideIndex() * 100
+  );
+
+  locationBgImg: string = '/assets/images/bg-2.jpg';
+  catalogueBgImg: string = '/assets/images/bg.png';
+
   ngOnInit(): void {
     this.images.set([
       {
         img: '/assets/images/home/home-1.jpg',
         name: 'WALL COLLECTION',
         sizes: ['12X18 mm', '12X24 mm'],
+        category: 'wall-collection'
       },
       {
         img: '/assets/images/home/home-2.jpg',
-        name: 'KITCHEN COLLECTION',
+        name: 'FLOOR COLLECTION',
         sizes: ['12X24 mm', '12X18 mm'],
+        category: 'floor-collection'
       },
       {
         img: '/assets/images/home/home-1.jpg',
-        name: 'BATHROOM COLLECTION',
+        name: 'PARKING COLLECTION',
         sizes: ['24X12 mm', '18X12 mm'],
+        category: 'parking-collection'
+      },
+    ]);
+
+    this.trendingImages.set([
+      {
+        img: '/assets/images/home/home-4.jpg',
+        name: 'BATHROOM FLOOR',
+      },
+      {
+        img: '/assets/images/home/home-1.jpg',
+        name: 'KITCHEN FLOOR',
+      },
+      {
+        img: '/assets/images/home/home-2.jpg',
+        name: 'BATHROOM FLOOR',
       },
     ]);
 
@@ -70,6 +105,29 @@ export class HomeComponent implements OnInit {
         img: '/assets/images/catalogues/5.jpg',
       },
     ]);
+
+    this.productImages.set([
+      {
+        name: 'The Ultima - 12X24 mm',
+        img: '/assets/images/catalogues/6.jpg',
+        desc: 'The Ultima, an extra-large Vitrified Slabs by Kajaria, is bigger than your dreams. While their large size exuberates the grandeur of real marble, their stunning looks charm connoisseurs across the globe.',
+      },
+      {
+        name: 'DuRock - 40X40 mm',
+        img: '/assets/images/catalogues/7.jpg',
+        desc: 'DuRock - Outdoor Ceramic Floor Tiles can be safely used in all internal and external spaces that are exposed to high load and DuRock - Outdoor Ceramic Floor Tiles can be safely used in all internal and external.',
+      },
+      {
+        name: 'Step Stone',
+        img: '/assets/images/catalogues/8.jpg',
+        desc: 'Ready to Install Vitrified Steps: Stairs and staircases are not defined by their functionality alone. These practical units are also interesting from a design perspective. Ready to Install Vitrified Steps.',
+      },
+      {
+        name: 'Vitronite',
+        img: '/assets/images/catalogues/3.jpg',
+        desc: 'New Age Modern Counter Tops: The incredible range of slabs are designed for modern application and to create a chic and urban aura for a space. New Age Modern Counter Tops.',
+      },
+    ]);
   }
 
   nextSlide() {
@@ -85,6 +143,22 @@ export class HomeComponent implements OnInit {
       this.currentSlideIndex.update((index) => index - 1);
     } else {
       this.currentSlideIndex.set(this.images().length - 1); // Loop to the last image
+    }
+  }
+
+  nextTrendingSlide() {
+    if (this.currentTrendingSlideIndex() < this.trendingImages().length - 1) {
+      this.currentTrendingSlideIndex.update((index) => index + 1);
+    } else {
+      this.currentTrendingSlideIndex.set(0); // Loop back to the first image
+    }
+  }
+
+  prevTrendingSlide() {
+    if (this.currentTrendingSlideIndex() > 0) {
+      this.currentTrendingSlideIndex.update((index) => index - 1);
+    } else {
+      this.currentTrendingSlideIndex.set(this.trendingImages().length - 1); // Loop to the last image
     }
   }
 }
