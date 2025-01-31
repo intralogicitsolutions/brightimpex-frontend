087cf19ffd31be2f4393cdcf1645060a4c8a95ce
Nikhil Dabhi <>
2025-01-31 18:59:57 +0530

    - contact us page desing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   apiRoot: 'http://localhost:3000/api',
+  phone: '1800 309 309',
+  mail: 'connect@brightimpex.com',
+  location: 'Morbi, Rajkot, Gujarat',
 };
