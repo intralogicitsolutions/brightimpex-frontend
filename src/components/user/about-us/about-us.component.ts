c1b46370a5e540a29ad98c6a809e82ced653d0a2
vaibhav <>
2025-02-17 17:52:38 +0530

    about-us-page-design

--- src/components/user/about-us/about-us.component.ts
+++ src/components/user/about-us/about-us.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from '../../../shared/animations/route-animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
   animations: [fadeAnimation],
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  tileBgImg: string = '/assets/images/tile.jpg';
+}
