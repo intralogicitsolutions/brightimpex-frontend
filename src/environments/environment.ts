0e201534b4942654241ba765009266931337bf82
vaibhav <>
2025-03-17 15:14:24 +0530

    footer-responsive-and-header-logo-center-done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
-  apiRoot: 'http://localhost:3000/api',
+  apiRoot: 'https://brightimpex-backend.onrender.com/api',
+  exportContact:{mail:'export@brightimpex.com', phone:'1800 309 309'},
+  domesticContact:{mail:'demestic@brightimpex.com', phone:'1700 500 402'},
   phone: '1800 309 309',
   mail: 'connect@brightimpex.com',
   location: 'Morbi, Rajkot, Gujarat',
